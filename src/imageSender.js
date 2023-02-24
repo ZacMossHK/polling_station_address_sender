acaeb59cbc4db28b8dd7b37705b01378d0a53ebe
Zac <zac@zacmoss.co.uk>
2023-02-24 20:37:16 +0000

    sendPollingStationMessage sends a message

--- src/imageSender.js
+++ src/imageSender.js
@@ -1,3 +1,23 @@
 require("dotenv").config();
 
-module.exports = class ImageSender {};
+module.exports = class ImageSender {
+  constructor(electoralCommisionApi, twilioApi) {
+    this.electoralCommisionApi = electoralCommisionApi;
+    this.twilioApi = twilioApi;
+  }
+
+  async sendPollingStationMessage(name, postcode, number, messageType) {
+    const pollingStationAddressInfo =
+      await this.electoralCommisionApi.getPollingStationAddressInfo(postcode);
+    const message = `Hi ${name},\n\nIt's time to vote! Your polling station is:\n\n${pollingStationAddressInfo.address.replace(
+      /, /g,
+      ",\n"
+    )}${
+      pollingStationAddressInfo.address.postcode
+        ? `\n${pollingStationAddressInfo.address.postcode}`
+        : ""
+    }`;
+    console.log(message);
+    return await this.twilioApi.sendWhatsAppMessage(message, number);
+  }
+};
--- test/imageSender.test.js
+++ test/imageSender.test.js
@@ -1,17 +1,33 @@
 const ImageSender = require("../src/imageSender");
 
-let imageSender, mockEcApi, fakeEcResponse;
+const votingMessage = "It's time to vote! Your polling station is:";
 
 describe("ImageSender class", () => {
-  beforeEach(() => {
-    fakeEcResponse = jest.fn();
-    mockEcApi = {
-      getPollingStationAddressInfo: jest.fn(async (postcode) =>
-        fakeEcResponse()
-      ),
+  it("successfully sends a WhatsApp message", async () => {
+    const mockTwilioApi = {
+      sendWhatsAppMessage: jest.fn().mockResolvedValue(true),
     };
-    imageSender = new ImageSender(mockEcApi);
+    const mockEcApi = {
+      getPollingStationAddressInfo: jest.fn().mockResolvedValue({
+        address: "Earlswood Social Club, 160-164 Greenway Road, Rumney",
+        postcode: "",
+      }),
+    };
+    const imageSender = new ImageSender(mockEcApi, mockTwilioApi);
+    const result = await imageSender.sendPollingStationMessage(
+      "name",
+      "postcode",
+      "number",
+      "whatsapp"
+    );
+    expect(mockEcApi.getPollingStationAddressInfo).toHaveBeenCalledWith(
+      "postcode"
+    );
+    const body = `Hi name,\n\n${votingMessage}\n\nEarlswood Social Club,\n160-164 Greenway Road,\nRumney`;
+    expect(mockTwilioApi.sendWhatsAppMessage).toHaveBeenCalledWith(
+      body,
+      "number"
+    );
+    expect(result).toBe(true);
   });
-
-  it("test to go here in the future", () => {});
 });
