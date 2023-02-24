b5aeac6e57d293b569a2a63504c9bbd22177af19
Zac <zac@zacmoss.co.uk>
2023-02-24 16:58:38 +0000

    removed google maps API reference

--- index.js
+++ index.js
@@ -5,6 +5,3 @@ require("dotenv").config();
 const ecApi = new ElectoralCommisionApi(
   process.env.ELECTORAL_COMMISION_API_KEY
 );
-const gMapsApi = new GoogleMapsApi(process.env.GOOGLE_MAPS_API_KEY);
-
-gMapsApi.getStreetViewImage([41.403609, 2.174448]);
