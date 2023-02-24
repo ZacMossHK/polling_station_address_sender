673f8043b7f45bd8cc3be174fb955abb67fd362e
Zac <zac@zacmoss.co.uk>
2023-02-24 17:00:27 +0000

    added Twilio API

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const ElectoralCommisionApi = require("./src/electoralCommisionAPI");
-const GoogleMapsApi = require("./src/googleMapsApi");
+const TwilioApi = require("./src/twilioApi");
 require("dotenv").config();
 
 const ecApi = new ElectoralCommisionApi(
   process.env.ELECTORAL_COMMISION_API_KEY
+);
+const twilioApi = new TwilioApi(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN,
+  process.env.TWILIO_FROM_NUMBER_WHATSAPP,
+  process.env.TWILIO_MESSAGING_SERVICE_SID
 );
