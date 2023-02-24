e36b9c1959beb5b27b36e20398cd38d65aa4f190
Zac <zac@zacmoss.co.uk>
2023-02-24 20:46:13 +0000

    moved mockEcApi into the describe section

--- test/imageSender.test.js
+++ test/imageSender.test.js
@@ -2,16 +2,18 @@ const ImageSender = require("../src/imageSender");
 
 const votingMessage = "It's time to vote! Your polling station is:";
 
+let mockEcApi;
 describe("ImageSender class", () => {
+  mockEcApi = {
+    getPollingStationAddressInfo: jest.fn().mockResolvedValue({
+      address: "Earlswood Social Club, 160-164 Greenway Road, Rumney",
+      postcode: "",
+    }),
+  };
+
   it("successfully sends a WhatsApp message", async () => {
     const mockTwilioApi = {
       sendWhatsAppMessage: jest.fn().mockResolvedValue(true),
-    };
-    const mockEcApi = {
-      getPollingStationAddressInfo: jest.fn().mockResolvedValue({
-        address: "Earlswood Social Club, 160-164 Greenway Road, Rumney",
-        postcode: "",
-      }),
     };
     const imageSender = new ImageSender(mockEcApi, mockTwilioApi);
     const result = await imageSender.sendPollingStationMessage(
@@ -30,4 +32,6 @@ describe("ImageSender class", () => {
     );
     expect(result).toBe(true);
   });
+
+  it("successfully sends an SMS message", () => {});
 });
