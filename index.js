ac8cd5f604ccab6f8653eb8f38089fc92a533d6e
Zac <zac@zacmoss.co.uk>
2023-02-24 09:51:42 +0000

    created GoogleMapsAPI

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const ElectoralCommisionApi = require("./src/electoralCommisionAPI");
+const GoogleMapsApi = require("./src/googleMapsApi");
 require("dotenv").config();
 
 const ecApi = new ElectoralCommisionApi(process.env.EC_API_KEY);
+const gMapsApi = new GoogleMapsApi(process.env.GOOGLE_MAPS_API_KEY);
+
+gMapsApi.getStreetViewImage([41.403609, 2.174448]);
