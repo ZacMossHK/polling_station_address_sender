3a86b072800bb581bb60947cdfa34cb5321920e4
Zac <zac@zacmoss.co.uk>
2023-02-25 22:00:31 +0000

    added imageSender and documentation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const ElectoralCommisionApi = require("./src/electoralCommisionAPI");
+const ImageSender = require("./src/imageSender");
 const TwilioApi = require("./src/twilioApi");
 require("dotenv").config();
 
+// all environmental variables must be set
 const ecApi = new ElectoralCommisionApi(
   process.env.ELECTORAL_COMMISION_API_KEY
 );
@@ -12,11 +14,17 @@ const twilioApi = new TwilioApi(
   process.env.TWILIO_MESSAGING_SERVICE_SID
 );
 
-twilioApi.sendWhatsAppMessage(
-  "Hey there! This is a message from the code.",
-  "+447983352662"
-);
-twilioApi.sendSmsMessage(
-  "Hey there! This is a message from the code.",
-  "+447983352662"
-);
+const imageSender = new ImageSender(ecApi, twilioApi);
+
+/* change the variables below to run
+name: string, name of the recipient (eg. John Smith)
+postcode: string, postcode of the recipient
+number: string, mobile number of the receipient with an international dialling code, eg. 0798... becomes +44798...
+messageType: string, type of message to send. Either "WhatsApp" or "Sms"
+*/
+let name;
+let postcode;
+let number;
+let messageType;
+
+imageSender.sendPollingStationMessage(name, postcode, number, messageType);
