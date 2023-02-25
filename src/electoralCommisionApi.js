7e5dbcc80d0023f66db584219a41f53537893466
Zac <zac@zacmoss.co.uk>
2023-02-25 21:45:24 +0000

    throws an error if address picker or no polling info is returned

--- src/electoralCommisionApi.js
+++ src/electoralCommisionApi.js
@@ -3,12 +3,24 @@ module.exports = class ElectoralCommisionApi {
     this.apiKey = apiKey;
   }
 
+  /* fetches a polling station information from the Electoral Commision Api
+  Args: [postcode: string]
+  Returns an object with polling station information if successful: {address: string, postcode: string}
+  Throws an error if fetch was unsuccessful,
+  or if an address picker is returned (postcode supplied matches several polling station areas),
+  or if no polling data exists
+  EC endpoint information and response examples: https://api.electoralcommission.org.uk/docs/
+  */
   async getPollingStationAddressInfo(postcode) {
     const response = await fetch(
       `https://api.electoralcommission.org.uk/api/v1/postcode/${postcode}?token=${this.apiKey}`
     );
     const result = await response.json();
-    if (!result.dates.length) return {};
+    if (!result.dates.length) {
+      if (result.address_picker)
+        throw Error("Electoral Commision API returned address picker");
+      throw Error("EC API returned no polling info");
+    }
     return result.dates[0].polling_station.station.properties;
   }
 };
