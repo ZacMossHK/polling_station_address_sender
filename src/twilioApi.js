95e7c29804100491ecd59c3afc0c35a600c345c5
Zac <zac@zacmoss.co.uk>
2023-02-25 23:57:17 +0000

    changed comment lines

--- src/twilioApi.js
+++ src/twilioApi.js
@@ -8,12 +8,9 @@ module.exports = class TwilioApi {
   }
 
   /* sends a message using the Twilio API and module
-
   Args: [createParams: object]
-  
   If a message is unsucessful the twilio module will throw an error, otherwise it will return an object
-  See here for response object examples: https://www.twilio.com/docs/usage/twilios-response
-  */
+  See here for response object examples: https://www.twilio.com/docs/usage/twilios-response */
   async sendMessage(createParams) {
     const message = await this.client.messages.create(createParams);
     console.log(`Message sent! SID: ${message.sid}`);
@@ -21,12 +18,9 @@ module.exports = class TwilioApi {
   }
 
   /* both these functions send a message to a chosen number
-
   Args: [body: string, toNumber: string]
   return value: bool, true if message sent succesfully, throws an error if not
-
-  toNumber must be prefixed with an international dialling code and no 0, eg. UK = 0798... => +44798...
-  */
+  toNumber must be prefixed with an international dialling code and no 0, eg. UK = 0798... => +44798... */
   async sendWhatsAppMessage(body, toNumber) {
     return await this.sendMessage({
       body,
